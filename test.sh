02a9ce3ea930d4ee83a552f115fb615384cddb57
Yuko Kimura <toranekoland@gmail.com>
2025-02-22 18:54:37 +0900

    Update

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 cd ~/poster-map/dev/ #Path to the folder
 
-git pull
+#git pull
 
 # Download latest CSV from spreadsheet datbase
-curl -sL "https://script.google.com/macros/s/AKfycbyaV4I5c6KdHMSwdBlLD-HbQM5wXPiZVlsUMEOSh__tWhlXoG621NuJrd6HIwjT_Cfn/exec?sheetName=conquerlist" > public/data/conquerlist.csv
-curl -sL "https://script.google.com/macros/s/AKfycbyaV4I5c6KdHMSwdBlLD-HbQM5wXPiZVlsUMEOSh__tWhlXoG621NuJrd6HIwjT_Cfn/exec?sheetName=conquerblock" > public/data/conquerblock.csv
+#curl -sL "https://script.google.com/macros/s/AKfycbyaV4I5c6KdHMSwdBlLD-HbQM5wXPiZVlsUMEOSh__tWhlXoG621NuJrd6HIwjT_Cfn/exec?sheetName=conquerlist" > public/data/conquerlist.csv
+#curl -sL "https://script.google.com/macros/s/AKfycbyaV4I5c6KdHMSwdBlLD-HbQM5wXPiZVlsUMEOSh__tWhlXoG621NuJrd6HIwjT_Cfn/exec?sheetName=conquerblock" > public/data/conquerblock.csv
 
 
 # all.json
@@ -19,13 +19,13 @@ curl -sL "https://script.google.com/macros/s/AKfycbyaV4I5c6KdHMSwdBlLD-HbQM5wXPi
 # summary_absolute.json
 #python3 summarize_progress_absolute.py ./public/data/summary_absolute.json
 
-#git add -N .
+git add -N .
 
-#if ! git diff --exit-code --quiet
-#then
-#    git add .
-#    git commit -m "Update"
-#    git push
+if ! git diff --exit-code --quiet
+then
+    git add .
+    git commit -m "Update"
+    git push
     #source .env
     #npx netlify-cli deploy --prod --message "Deploy" --dir=./public --auth $NETLIFY_AUTH_TOKEN
-#fi
+fi
