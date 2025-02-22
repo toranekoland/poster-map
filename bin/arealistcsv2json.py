b23e14d3686a74fa3f35d0447a89274a4957e8a9
Yuko Kimura <toranekoland@gmail.com>
2025-02-22 16:30:08 +0900

    Update

---
+++ bin/arealistcsv2json.py
@@ -0,0 +1,23 @@
+# エリアリストをjsonに変換
+# area_id,area_name,area_block
+
+import pandas as pd
+import sys
+import os
+
+def main(input_path, output_path):
+
+    inputdata = pd.read_csv(input_path)
+    inputdata = inputdata.set_index('area_id')
+    json_data = inputdata.to_json(output_path, orient='index',force_ascii=False)
+    print(f"File saved to {output_path}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_path> <output_path>")
+        sys.exit(1)
+
+    input_path = sys.argv[1]  # public/data/arealist.csv
+    output_path = sys.argv[2] # public/data/arealist.json
+
+    main(input_path, output_path)
--- bin/conquercsv2json_small.py
+++ bin/conquercsv2json_small.py
@@ -1,3 +1,10 @@
+## ポスティングデータをエリアごとに分割しjsonに出力する
+# ポスティングマップデータ id(連番),エリア名(自治体名),エリア名(住所),トータル枚数,最近の枚数,備考
+# conquerlist.csv id,area_name,subarea_name,total_posting,recently_posting,note
+# ブロックデータ id(連番),エリアキー(ローマ字),エリア名(自治体名)
+# conquerblock.csv area_id,area_key,area_name
+# python3 bin/conquercsv2json_small.py public/data/conquerlist.csv public/data/conquerblock.csv public/data/
+
 import pandas as pd
 import sys
 import os
@@ -13,23 +20,18 @@ def main(input_path, block_path, output_path):
 
     merged_data = pd.merge(inputdata, blocklist, on='area_name', how='left', suffixes=('', '')) # left_joinでマージ
     final_data = merged_data.copy()[['id', 'area_id', 'area_key', 'subarea_name', 'total_posting', 'recently_posting', 'note']] # 必要なデータのみ取り出し
-    print(final_data)
    
     for area_key, area_name in conquer_blocks.items():
-        print(area_key)
-        print(area_name)
         block_areas = blocklist[blocklist['area_name'] == area_name]['area_id']
         filtered_data = final_data[final_data['area_id'].isin(block_areas)]
-        print(filtered_data)
-        
+        filtered_data = filtered_data[['subarea_name', 'total_posting', 'recently_posting', 'note']] # 地名、トータルポスティング枚数、最近のポスティング枚数、備考
         filtered_output_path = os.path.join(output_path, 'conquer', f'{area_key}.json')
-        print(filtered_output_path)
         filtered_data.to_json(filtered_output_path, orient='records', force_ascii=False)
         print(f"Filtered file saved to {filtered_output_path}")
 
-#    json_output_path = os.path.join(output_path, 'all.json')
-#    final_data.to_json(json_output_path, orient='records', force_ascii=False)
-#    print(f"File saved to {json_output_path}")
+    json_output_path = os.path.join(output_path, 'conquer.json')
+    final_data.to_json(json_output_path, orient='records', force_ascii=False)
+    print(f"File saved to {json_output_path}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
