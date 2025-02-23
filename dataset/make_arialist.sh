a4c7ffacdc64730b3a6d83b068338a3783b175b6
Yuko Kimura <toranekoland@gmail.com>
2025-02-23 22:30:22 +0900

    Update

--- dataset/make_arialist.sh
+++ dataset/make_arialist.sh
@@ -2,3 +2,11 @@
 set -euo pipefail
 
 # 神奈川県の地名リストを出力（負荷が高いので、一回だけね）
+
+python3 all_arealist.py "神奈川県" 1 kanagawa.csv
+python3 all_arealist.py "神奈川県横浜市" 1 kanagawa-yokohama.csv
+python3 all_arealist.py "神奈川県川崎市" 1 kanagawa-kawasaki.csv
+python3 all_arealist.py "神奈川県相模原市" 1 kanagawa-sagamihara.csv
+
+# csvを加工してarealist.csvを作るのです
+# area_id,area_name,area_block area_blockは任意にグルーピングしてください
