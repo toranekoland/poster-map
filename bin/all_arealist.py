a1693c70007929129129e8292c01b9f3959239a6
Yuko Kimura <toranekoland@gmail.com>
2025-02-23 20:00:18 +0900

    Update

--- bin/all_arealist.py
+++ bin/all_arealist.py
@@ -9,16 +9,13 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger()
 
 # 基本URL
-base_url = "https://uedayou.net/loa/神奈川県鎌倉市寺分"
+base_url = "https://uedayou.net/loa/神奈川県鎌倉市"
 
 # すでに取得したURLを保持して、重複リクエストを防ぐ
 visited_urls = set()
 
 # 初期データの列名を設定
 columns = ['Full URL', 'Stripped URL', 'Level']
-
-# 取得したURLを保持するリストを作成（pandasのDataFrame用）
-#all_urls = []
 
 # 再帰的にデータを取得する関数
 def get_child_data(url, level=0):  # levelパラメータを追加して再帰階層を追跡
@@ -51,8 +48,6 @@ def get_child_data(url, level=0):  # levelパラメータを追加して再帰�
         df = pd.DataFrame([[url, stripped_url, level]], columns=columns)
         df.to_csv('urls_output.csv', mode='a', header=False, index=False, encoding='utf-8')
         logger.info(f"Appended URL: {url}, stripped_url: {stripped_url},Level: {level}")
-        with open('urls_output.csv', mode='a', encoding='utf-8') as file:
-            file.flush()  # ファイルにデータを即座に書き込む
 
         # 子供のデータを再帰的に取得
         for item in has_part:
@@ -77,10 +72,8 @@ if response.status_code == 200:
 
     # **初期データ部分もall_urlsに追加**（階層0として追加）
     df = pd.DataFrame([[target_url, "", 0]], columns=columns)
-    df.to_csv('urls_output.csv', mode='a', header=False, index=False, encoding='utf-8')
+    df.to_csv('urls_output.csv', mode='w', header=True, index=False, encoding='utf-8')
     logger.info(f"Appended Initial URL: {target_url}, Level: 0")
-    with open('urls_output.csv', mode='a', encoding='utf-8') as file:
-        file.flush()  # ファイルにデータを即座に書き込む
 
     # 'hasPart'に該当する部分を抽出
     has_part = data.get(base_url, {}).get("http://purl.org/dc/terms/hasPart", [])
@@ -96,8 +89,4 @@ else:
     print(f"データの取得に失敗しました。ステータスコード: {response.status_code}")
     print(f"Response Headers: {response.headers}")
 
-# pandasでCSVに出力する部分
-# pandasのDataFrameにURLリストを渡してCSV出力
-#df = pd.DataFrame(all_urls, columns=['Full URL', 'Stripped URL', 'Level'])  # 'Full URL', 'Stripped URL', 'Level'の3列
-#df.to_csv('urls_output.csv', index=False, encoding='utf-8')  # CSVファイルに保存
 print("CSVファイルにURLを書き込みました。")
