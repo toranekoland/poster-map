97ca6b47750847b830db9ab485b8b214031312dc
Yuko Kimura <toranekoland@gmail.com>
2025-02-24 11:30:21 +0900

    Update

--- dataset/make_arialist.sh
+++ dataset/make_arialist.sh
@@ -16,7 +16,8 @@ python3 all_arealist.py "神奈川県横浜市都筑区" 3 "kanagawa/tsuzuki.csv
 python3 all_arealist.py "神奈川県川崎市川崎区" 3 "kanagawa/kawasaki.csv"
 python3 all_arealist.py "神奈川県川崎市幸区" 3 "kanagawa/saiwai.csv"
 python3 all_arealist.py "神奈川県川崎市中原区" 3 "kanagawa/nakahara.csv"
-python3 all_arealist.py "神奈川県川崎市高津区" 3 "kanagawa/takatsu.csv"
+#python3 all_arealist.py "神奈川県川崎市高津区" 3 "kanagawa/takatsu.csv"
 python3 all_arealist.py "神奈川県川崎市多摩区" 3 "kanagawa/tama.csv"
 python3 all_arealist.py "神奈川県川崎市宮前区" 3 "kanagawa/miyamae.csv"
-python3 all_arealist.py "神奈川県横浜市戸塚区" 3 "kanagawa/totsuka.csv"
+#python3 all_arealist.py "神奈川県横浜市戸塚区" 3 "kanagawa/totsuka.csv"
+
